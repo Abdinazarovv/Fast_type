45ab8d0a50d6891090d96e1e59d02b1e8bf2235d
Abdinazarovv <islombekabdinazarov07@gmail.com>
2022-08-04 13:06:01 +0300

    main things finish

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-const word = document.getElementById('word');
-const text = document.getElementById('input_area');
-const scoreE1 = document.getElementById('score');
-const timeE1 = document.getElementById('end_gane');
+// elements
+const word = document.getElementById('word'); //h3
+const text = document.getElementById('input_area'); // input
+const scoreEl = document.getElementById('score');
+const timeEl = document.getElementById('time');
+const endGameEl = document.getElementById('end_game');
 
 const words = [
     "npm cache",
@@ -88,9 +90,54 @@ const words = [
 ]
 
 let randomWord;
+
 let score = 0;
+
 let time = 60;
 
 function getRandomWord() {
-    return words[Math.floor(Math.random()* words.length)]
+    return words[Math.floor(Math.random() * words.length)]
 }
+
+function addToDom() {
+    randomWord = getRandomWord()
+    word.innerHTML = randomWord;
+}
+
+function updateScore() {
+    score++;
+    scoreEl.innerHTML = score;
+}
+
+function updateTime() {
+    time--;
+    timeEl.innerHTML = time + "s";
+    
+    if(time === 0) {
+        clearInterval(timeInterval)
+        gameOver();
+    }
+}
+const timeInterval = setInterval(updateTime, 1000);
+
+function gameOver() {
+    endGameEl.innerHTML = `
+    <h1>Vaqtingiz tugadi!</h1>
+    <h3>Sizning balingiz: ${score}</h3>
+    <button onclick="location.reload()">Yangilash</button>
+    `
+    endGameEl.style.display = "flex";
+}
+
+addToDom();
+
+text.addEventListener("input", (e) => {
+    const typedText = e.target.value;
+    
+    if(typedText === randomWord){
+        updateScore(); // ++1
+        addToDom();
+        e.target.value = "";
+        updateTime();
+    }
+})
